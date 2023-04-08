388f0c250ebc03a9936dffc6178362537a936691
Ira Akulova <ira.akulova2019@gmail.com>
2023-04-08 11:18:06 +0300

    fixed goback btn action

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect} from 'react';
 import { Link, useSearchParams, useLocation } from 'react-router-dom';
-import _debounce from 'debounce';
 
 const KEY = '136e9303af57d83b29ddf02ef48e9efe';
 const BASE_URL = `https://api.themoviedb.org/3/search/movie?api_key=${KEY}`;
@@ -9,27 +8,29 @@ const Movies = () => {
   const [films, setFilms] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
   const [visibleList, setVisibleList] = useState(false);
+  const [inputValue, setInputValue] = useState('');
   const query = searchParams.get('query');
   const location = useLocation();
 
   const onInputChange = (e) => {
-    setSearchParams({ query: e.target.value });
+    setInputValue(e.target.value);
   };
-
-  useEffect(() => {
-        setVisibleList(false);
-        fetch(
-          `${BASE_URL}&query=${query}&language=en-US&page=1&include_adult=false`
-        )
-          .then(response => response.json())
-          .then(films => setFilms(films.results));
-  }, [query]);
 
   const onFormSubmit = (e) => {
     e.preventDefault();
+    setSearchParams({ query: inputValue });
     setVisibleList(true);
-
   };
+
+  useEffect(() => {
+    fetch(`${BASE_URL}&query=${query}&language=en-US&page=1&include_adult=false`)
+      .then(response => response.json())
+      .then(films => setFilms(films.results));
+  }, [query]);
+
+  useEffect(() => {
+    setVisibleList(true);
+  }, []);
   
     return (
       <div>
@@ -39,13 +40,15 @@ const Movies = () => {
             autoComplete="off"
             autoFocus
             placeholder="Search movies"
-            onChange={_debounce(onInputChange, 300)}
+            onChange={onInputChange}
+            value={inputValue}
           ></input>
           <button type="submit">Search</button>
         </form>
 
         <ul>
           {visibleList &&
+            query &&
             films.map(film => {
               return (
                 <li key={film.id}>
