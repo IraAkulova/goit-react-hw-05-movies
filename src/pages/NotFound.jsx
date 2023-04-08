29f96c1c4b27b09c16e5719955f2afdcb5e58664
Ira Akulova <ira.akulova2019@gmail.com>
2023-04-08 19:29:59 +0300

    11

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,17 @@
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div>
+      <h1
+        style={{
+          marginLeft: 500,
+          marginTop: 50,
+          color: 'blue',
+        }}
+      >
+        404 Not Found
+      </h1>
+    </div>
+  );
 };
 
 export default NotFound;
