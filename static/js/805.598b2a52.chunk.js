f80b256cd446bb1aced2f1504576c1fe5b03c78f
IraAkulova <99876200+IraAkulova@users.noreply.github.com>
2023-04-08 16:31:17 +0000

    Deploying to gh-pages from @ IraAkulova/goit-react-hw-05-movies@29f96c1c4b27b09c16e5719955f2afdcb5e58664 🚀

---
+++ static/js/805.598b2a52.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[805],{805:function(e,t,i){i.r(t);var n=i(184);t.default=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("h1",{style:{marginLeft:500,marginTop:50,color:"blue"},children:"404 Not Found"})})}}}]);
+//# sourceMappingURL=805.598b2a52.chunk.js.map
